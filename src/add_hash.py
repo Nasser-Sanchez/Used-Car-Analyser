c3408a64c01ef1ea3d90c46497631732624ecae6
Nasser-Sanchez <jameel02@live.co.uk>
2026-08-25 23:22:05 +0100

    Update add_hash.py

--- src/add_hash.py
+++ src/add_hash.py
@@ -7,8 +7,8 @@ import re
 import os
 import csv
 
-df = pd.read_csv("carmax_USA.csv")
-df.to_csv('carmax_USA copy.csv', index=False)
+df = pd.read_csv("data/carmax_USA.csv")
+df.to_csv('data/armax_USA copy.csv', index=False)
 # 2. Create a consistent string key (lowercase to avoid case-sensitivity issues)
 df['car_key'] = (df['year'].astype(str) + '-' + 
                  df['make'].str.lower() + '-' + 
@@ -20,5 +20,5 @@ df['car_key'] = (df['year'].astype(str) + '-' +
 df['car_id'] = df['car_key'].apply(lambda x: int(hashlib.md5(x.encode()).hexdigest(), 16))
 
 # 4. Save as a new file (so you don't lose the original)
-df.to_csv('carmax_USA.csv', index=False)
+df.to_csv('data/carmax_USA.csv', index=False)
 print("Done! You now have a 'car_id' column in your CSV.")
